7dcd4ffbad1aeb0498669e600975497a607ca247
daypark18 <vmdaypark@gmail.com>
2021-01-20 07:23:59 -0500

    another commit

--- js/script.js
+++ js/script.js
@@ -67,5 +67,59 @@ $(function(){
 
     });
 
+    $(".page").each(function (index, element) {
+
+    $(element).on("mousewheel DOMMouseScroll", function (e) {
+      //console.log($(this).html());
+
+      // console.log("e ====", e);
+      // console.log("originalEvent ====", e.originalEvent);
+
+      e.preventDefault();
+
+      var El = e.originalEvent;
+
+      var delta = 0;
+
+
+      if (El.wheelDelta) {
+        delta = event.wheelDelta / 120;
+        //if (window.opera) delta = -delta;
+      } else if (El.detail) delta = -El.detail / 3;
+
+
+      if(parell.mnoving == false) {
+        parell.mnoving = true;
+
+        // 마우스휠을 위에서 아래로
+        if (delta  < 0 ) {
+          if($(window).scrollTop() == parell.distance * (parell.length-1)) {
+            moveTop = $(this).offset().top;
+          } else {
+            moveTop = $(this).next().offset().top;
+          }
+        // 마우스휠을 아래에서 위로
+        } else {
+          if($(window).scrollTop() < parell.distance) {
+             moveTop = $(this).offset().top;
+          } else {
+            moveTop = $(this).prev().offset().top;
+          }
+        }
+
+
+        $("html,body").stop().animate({
+          scrollTop: moveTop + 'px'
+        }, {
+          duration: 800, complete: function () {
+            parell.mnoving = false;
+          }
+        });
+      }
+
+    });
+  });
+
+
 
 })
